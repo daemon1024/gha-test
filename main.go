dff734dfc4945be0a272ba2149b7f89ed5caa068
daemon1024 <barun1024@gmail.com>
2023-02-06 14:44:46 +0530

    update bpflsm test gha
    Signed-off-by: daemon1024 <barun1024@gmail.com>

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/features"
 	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
+	"golang.org/x/sys/unix"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang bpf test.bpf.c -- -I/usr/include/bpf -O2 -g
+
+type eventBPF struct {
+	Comm [80]uint8
+}
 
 func main() {
 	err := features.HaveProgramType(ebpf.LSM)
@@ -35,9 +48,54 @@ func main() {
 	}
 	defer kpbprm.Close()
 
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-	for range ticker.C {
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
 
+	rd, err := ringbuf.NewReader(objs.Events)
+	if err != nil {
+		log.Fatalf("opening ringbuf reader: %s", err)
+	}
+	defer rd.Close()
+
+	go func() {
+		<-stopper
+
+		if err := rd.Close(); err != nil {
+			log.Fatalf("closing ringbuf reader: %s", err)
+		}
+	}()
+
+	go func() {
+		for {
+			_, _ = exec.Command("sleep", "1").Output()
+		}
+	}()
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		if err := rd.Close(); err != nil {
+			log.Fatalf("closing ringbuf reader: %s", err)
+		}
+		os.Exit(0)
+	}()
+
+	var event eventBPF
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			if errors.Is(err, ringbuf.ErrClosed) {
+				log.Println("Received signal, exiting..")
+				return
+			}
+			log.Printf("reading from reader: %s", err)
+			continue
+		}
+
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+			log.Printf("parsing ringbuf event: %s", err)
+			continue
+		}
+
+		fmt.Printf("Exec %s \n", unix.ByteSliceToString(event.Comm[:]))
 	}
 }
--- test.bpf.c
+++ test.bpf.c
@@ -8,8 +8,28 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
+typedef struct {
+  u8 comm[80];
+} event;
+struct {
+  __uint(type, BPF_MAP_TYPE_RINGBUF);
+  __uint(max_entries, 1 << 24);
+} events SEC(".maps");
+
 SEC("lsm/bprm_check_security")
 int BPF_PROG(bprm_stuff, struct linux_binprm *bprm, int ret) {
-  bpf_printk("exec of %s", bprm->filename);
+  //   bpf_printk("exec of %s", bprm->filename);
+
+  event *fn;
+
+  fn = bpf_ringbuf_reserve(&events, sizeof(event), 0);
+  if (!fn) {
+    return 0;
+  }
+
+  bpf_probe_read_str(&fn->comm, 80, (void *)bprm->filename);
+
+  bpf_ringbuf_submit(fn, 0);
+
   return ret;
 }
